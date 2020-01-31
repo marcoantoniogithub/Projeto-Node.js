8d453aeceaa6bf2048965b137cc9065b2e0511b5
Marco Antonio <mrodrigues@scopus.com.br>
2020-01-31 15:20:18 -0300

    configuração sequelize

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: 'root',
+  password: '123456',
+  database: 'databaseteste',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+}
--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Started aaa");
-})
+const { User } = require('./app/models');
 
-app.get("/contato", function(req, res){
-    res.send("pagina contato");
-})
-app.listen(8080);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// how to create new user
+//User.create({ name: 'Claudio', email: 'claudio@rocketseat.com.br', password: '123456' });
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/register', async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+app.get('/find/:id', (req, res) => {
+  res.json('Im in register');
+});
+
+app.get('/findall', (req, res) => {
+  res.json('Im in Find All');
+});
+
+app.put('/update/:id', (req, res) => {
+  res.json('Im in update');
+});
+
+app.delete('/delete/:id', (req, res) => {
+  res.json('Im in delete'+id);
+});
+
+app.listen(3100);
